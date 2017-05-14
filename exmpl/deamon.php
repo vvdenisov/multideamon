73f40ac54ab40ed44c0deca379821e774cefbdc7
Vadim <vvdenisov@mail.ru>
2017-05-14 18:00:10 +0300

    translate ))

--- exmpl/deamon.php
+++ exmpl/deamon.php
@@ -1,12 +1,6 @@
 #!/usr/bin/env php
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 set_time_limit(0);
 date_default_timezone_set('Europe/Moscow');
 error_reporting(E_ALL);
